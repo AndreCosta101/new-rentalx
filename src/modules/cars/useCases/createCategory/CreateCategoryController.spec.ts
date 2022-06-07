a599a95ef8bdc8567f00b3c4b02a23160e2dcc69
AndreCosta <andre.costa101@gmail.com>
2022-06-07 17:23:19 -0300

    Criando o primeiro teste de integraçao

--- src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
@@ -2,8 +2,14 @@ import request from 'supertest';
 
 import { app } from '../../../../shared/infra/http/app';
 
-describe('Create Category Controller', async() => {
-    it('test', async() => {
-        await request(app).get('/cars/available').expect(200);
+describe('Create Category Controller', () => {
+    it('should be able to create a new category', async() => {
+        const response =  await request(app).post('/categories')
+            .send({
+                name: 'Category Supertest', 
+                description: 'Category Supertest'
+            });
+
+            expect(response.status).toBe(200); 
     })
 }) 
