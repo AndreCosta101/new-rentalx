1a13ae713276fade3084a3309722c4bca00edd44
AndreCosta <andre.costa101@gmail.com>
2022-07-27 15:30:45 -0300

    Provider de Upload

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -3,6 +3,9 @@ import { IDateProvider } from '../providers/DateProvider/IDateProvider';
 import { DayjsDateProvider } from '../providers/DateProvider/implementations/DayjsDateProvider';
 import { IMailProvider } from './MailProvider/IMailProvider';
 import { EtherealMailProvider } from './MailProvider/implementations/EtherealMailProvider';
+import { LocalStorageProvider } from './StorageProvider/implementations/LocalStorageProvider';
+import { IStorageProvider } from './StorageProvider/IStorageProvider';
 
 container.registerSingleton<IDateProvider>('DayjsDateProvider', DayjsDateProvider)
 container.registerInstance<IMailProvider>('EtherealMailProvider', new EtherealMailProvider())
+container.registerSingleton<IStorageProvider>('StorageProvider', LocalStorageProvider)
